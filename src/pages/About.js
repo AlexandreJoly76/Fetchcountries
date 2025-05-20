74138ed083d036fa790587159199799a12978b94
ajoly <alexandre.joly.76300@gmail.com>
2025-05-20 10:17:47 +0200

    fetchcountries

---
+++ src/pages/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+        <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti eos
+        inventore, aperiam vel praesentium doloribus quis minus ullam corporis.
+        Impedit doloremque facere ipsum nihil quidem consequatur repellendus
+        optio, error ipsa doloribus cumque, eius sint laudantium dignissimos
+        itaque soluta, molestiae vero dolore repudiandae consequuntur iste!
+        Rerum dolorem nesciunt perspiciatis nostrum error ratione distinctio!
+        Voluptas aut quod ad magni expedita saepe architecto, quaerat recusandae
+        facere asperiores laboriosam sequi facilis incidunt magnam et. At esse
+        delectus vero accusamus sapiente eos asperiores ut, quidem aliquid
+        laborum dolorem atque recusandae tempora saepe dignissimos officia
+        provident, soluta quia impedit? Rerum sint atque non sed ab nisi?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius saepe
+        provident maxime delectus nulla dolore quod voluptatibus placeat soluta,
+        expedita minus accusantium culpa. Odio sed omnis asperiores temporibus
+        culpa delectus iure molestiae commodi quidem esse qui ea perferendis
+        deleniti rerum hic est, eum nam. Laboriosam fugiat numquam esse
+        laudantium. Quam ab hic, non exercitationem amet nemo? Saepe, ducimus.
+        Optio animi accusamus earum quod? Eveniet inventore necessitatibus enim
+        nobis culpa rem facere optio dolores? Tempora natus, accusamus, enim
+        doloribus ut blanditiis qui sequi dolorum odio, maiores ipsum iure.
+        Ducimus voluptatem, cum iste voluptatibus reprehenderit repellat
+        consequuntur nostrum quis maiores. Suscipit, eum.
+      </p>
+    </div>
+  );
+};
+
+export default About;
